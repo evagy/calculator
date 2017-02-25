12529602617666f7c836c9d87309f88d827b9c9b
yuyanggong <421736079@qq.com>
2017-02-25 16:29:06 +0800

    demo complete

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,24 @@
 // 接受一个需要解析的字符串， 
 // 如果正确，返回一个结果字符串
-// 如果错误，则返回一个length为2的数组，数组中第一个元素代表错误代码
-// 第二个代表错误信息
+// 如果错误，则返回一个错误字符串
 function caculate (str) {
 	var curStr = str.replace(/\s/g, ''), res, err;
 
 	// 如果匹配到非法字符就直接返回错误
 	if(/[^\w()+\-*/\.π\^,%]/.test(curStr)) {
 		console.log('初始匹配出现的问题');
-		return [0, '输入的算式格式不正确'];
+		return '输入的算式格式不正确';
 	}
+
 	
 	// 将方法名替换成挂载在全局变量cacu上的相关方法名
-	// 过程中如果发现未定义的方法，则手动设置err为true
-	curStr = curStr.replace(/\b[a-z]+\b/gi, (v, i) => {
-		if(replaceMethods[v]) return `cacu.${v}`;
+	// 过程中如果发现未定义的方法，则手动设置err
+	curStr = curStr.replace(/(\b[a-z]+\b)|π/gi, (v) => {
+		if(replaceMethods[v]) return `replaceMethods.${v}`;
 		else err = true;
 	});
-	if(err) return [1, `${i}处的算术方法不存在`];
+	console.log(curStr);
+	if(err) return `算术方法不存在`;
 
 	// 将^幂运算替换成 Math.pow
 	// 底数和次方数可能的情况大致有四种 
@@ -25,9 +26,9 @@ function caculate (str) {
 	// 2: 包括左右括号的数字运算式 (eg: (4 + 4))
 	// 3: 全局算术方法运算 (eg: Math.sin(5))
 	// 4：以上混合 (eg: ( Math.sin(5+6) + Math.cos(3-1) + 5 + (5-1) ) )
-	// 但是这里只考虑第一种和第二种。
+	// 但是这里只考虑第一种和第二种。第三种和第四种以后考虑。
 	if(/\^/.test(curStr)) {
-		curStr = curStr.replace(/\b(\(?\d+\)?)\^(\(?\d+\)?)\b/g, (v, a, b) => `cacu.pow($(a),${b})`);
+		curStr = curStr.replace(/\b(\(?\d+\)?)\^(\(?\d+\)?)\b/g, (v, a, b) => `replaceMethods.pow(${a},${b})`);
 	}
 
 	// 尝试用eval解析， 如果解析失败则捕捉错误并返回错误代码
@@ -39,21 +40,25 @@ function caculate (str) {
 		console.log(curStr)
 		return [0, '输入的算式格式不正确'];
 	}
-	// 只精确到小数点后16位， 避免双浮点精度问题
-	return +res.toFixed(16);
-	// return res.indexOf('Infinity') > -1 ? res.replace(/Infinity/, 'n') : res;
+	// 只精确到小数点后15位， 避免双浮点精度丢失
+	// 15位防止Math.sin精度丢失 16位防止0.1+0.2这种情况
+    return (res = res+'').indexOf('Infinity') > -1 ? res.replace(/Infinity/, 'n') : +(+res).toFixed(15);
+}
+
+let status = {
+	isAngle: true
 }
 
 // 一个储存自己数学方法的仓库
 let util = {
-	avg: arr => arr.map(Number).reduce((s, v) => s + v, 0) / arr.length,
+	avg: (...arr) => arr.map(Number).reduce((s, v) => s + v, 0) / arr.length,
 
 	cot: num => Math.cos(num) / Math.sin(num),
 	//存在浮点型精度问题。
 	log: (num1, num2) => Math.log(num1) / Math.log(num2),
 
 	log10: function(num) {
-		return this.log(num, 10);
+		return util.log(num, 10);
 	},
 	
 	acosh: Math.acosh || function(num) {return Math.log(num + Math.sqrt(num * num - 1))},
@@ -67,18 +72,30 @@ let util = {
 
 	sum: (a, b) => a + b,
 
-	pow: Math.pow
+	pow: Math.pow,
+
+	angle2rad: n => n * 2 * Math.PI / 360,
+
+	rad2angle: n => n * 180 / Math.PI
 }
 // 对于JS中Math对象中有的方法进行替换， 没有的方法则自己实现
 let replaceMethods = {
 	// 导师给定需要实现的方法。
 	'abs': Math.abs,
 	'avg': util.avg,
+
 	'sin': Math.sin,
 	'cos': Math.cos,
 	'tan': Math.tan,
 	'cot': util.cot,
 
+	'acos': Math.acos,
+	'acosh': util.acosh,
+	'asin': Math.asin,
+	'atan': Math.atan,
+	'asinh': util.asinh,
+	'atanh': util.atanh,
+	'atan2': Math.atan2,
 	// ES6中Math对象上有等同性质的函数Math.log10
 	//但是根据MDN(https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Math/log10)
 	//大多浏览器(新版本chrome上支持良好)不予支持, 故自己实现
@@ -90,22 +107,32 @@ let replaceMethods = {
 	'π': Math.PI,
 
 	// 个人实现的方法
-	'acos': Math.acos,
-	'acosh': util.acosh,
-	'asin': Math.asin,
 	'logMutil' : util.log,
-	'atan': Math.atan,
-	'asinh': util.asinh,
-	'atanh': util.atanh,
-	'atan2': Math.atan2,
 	'cbrt': util.cbrt,
 	'sum': util.sum,
 	'pow': util.pow
-}
+};
+
+// 将方法中的三角函数类方法再加工，根据设置(status.isAngle)
+// 判断输入是角度还是弧度
+[	'sin',
+	'cos',
+	'tan',
+	'cot',
+	'acos',
+	'acosh', 
+	'asin',
+	'atan',
+	'asinh', 
+	'atanh', 
+	'atan2'  ].map(v => {
+		var oriFn = replaceMethods[v];
+		replaceMethods[v] = n => status.isAngle ? oriFn(util.angle2rad(n)) : oriFn(n);
+	});
 
 // 挂载到全局上， 方便eval内部使用
-//	window.cacu = caculate;
-caculate.__proto__ = replaceMethods;
+//window && (window.cacu = caculate);
+
 
 function addCustomMethods (name, str) {
 	replaceMethods[name] = function(...arg) {
--- test/main.test.js
+++ test/main.test.js
@@ -3,33 +3,102 @@ let assert = require('assert'),
 
 let equal = assert.strictEqual;
 
-describe('单项测试', () => {
-	describe('加法运算', () => {
-			it('int型加法运算', () => {
-				var num1, num2;
-				for(let i = 0; i < 10; i++) {
-					num1 = Math.random()*1000|0;
-					num2 = Math.random()*1000|0;
-					equal(ca(` ${num1} + ${num2} `), +(num1 + num2).toFixed(16));
-				}
-			});
-			it('float型加法运算', () => {
-				var num1, num2;
-				for(let i = 0; i < 10; i++) {
-					num1 = Math.random();
-					num2 = Math.random();
-					equal(ca(` ${num1} + ${num2} `), +(num1 + num2).toFixed(16));
-				}
-			});
-			it('float型与int形混合加法运算', () => {
-				var num1 = Math.random(), 
-					num2 = Math.random() / Math.random() | 0;
-				for(let i = 0; i < 20; i++) {
-					equal(ca(`${num1}+${num2}`), num1 + num2);
-				}
-			});
-			it('float型精度丢失', () => {
-				equal(ca('0.1+0.2'), 0.3);
-			})
+const sum = arr => arr.reduce((s, v) => s + v, 0);
+
+
+describe('简单测试', () => {
+	it('加法', () => {
+		equal(ca('1+2'), 3);
+	});
+	it('减法', () => {
+		equal(ca('1-2'), -1);
+	});
+	it('乘法', () => {
+		equal(ca('2*2'), 4);
+	});
+	it('除法', () => {
+		equal(ca('4/3'), +(4/3).toFixed(15))
+	});
+	it('乘方', () => {
+		equal(ca('2^3'), 8);
+	});
+	it('取余', () => {
+		equal(ca('3%2'), 1);
 	})
-})
+	it('绝对值运算', () => {
+		equal(ca('abs(-1)'), 1);
+	})
+	it('均值运算', () => {
+		equal(ca('avg(1,2)'), 1.5);
+	});	
+	it('正弦运算', () => {
+		equal(ca('sin(30)'), 0.5);
+	})
+	it('余弦运算', () => {
+		equal(ca('cos(60)'), 0.5);
+	})
+	it('正切运算', () => {
+		equal(ca('tan(45)'), 1);
+	});
+	it('余切运算', () => {
+		equal(ca('cot(45)'), 1);
+	});
+	it('log底对数运算', () => {
+		equal(ca('log(100)'), 2);
+	});
+	it('ln自然对数', () => {
+		equal(ca('ln(100)'), Math.log(100));
+	});
+	it('e自然常量', () => {
+		equal(ca('e'), Math.E);
+	});
+	it('π自然常量', () => {
+		equal(ca('π'), Math.PI);
+	});
+	// describe('加法运算', () => {
+	// 		it('int型加法运算', () => {
+	// 			var num1, num2;
+	// 			for(let i = 0; i < 10; i++) {
+	// 				num1 = Math.random()*1000|0;
+	// 				num2 = Math.random()*1000|0;
+	// 				equal(ca(` ${num1} + ${num2} `), +(num1 + num2).toFixed(15));
+	// 			}
+	// 		});
+	// 		it('float型加法运算', () => {
+	// 			var num1, num2;
+	// 			for(let i = 0; i < 10; i++) {
+	// 				num1 = Math.random();
+	// 				num2 = Math.random();
+	// 				equal(ca(` ${num1} + ${num2} `), +(num1 + num2).toFixed(15));
+	// 			}
+	// 		});
+	// 		it('float型与int形混合加法运算', () => {
+	// 			var num1 = Math.random(), 
+	// 				num2 = Math.random() / Math.random() | 0;
+	// 			for(let i = 0; i < 20; i++) {
+	// 				equal(ca(`${num1}+${num2}`), +(num1 + num2).toFixed(15));
+	// 			}
+	// 		});
+	// 		it('连续整形加法运算', () => {
+	// 			var arr;
+	// 			for(let i = 0; i < 10; i++) {
+	// 				arr = new Array(Math.random()*10|0).fill(Math.random())
+	// 			}
+	// 		});
+	// })
+});
+
+describe('边界情况测试', () => {
+	it('float型精度丢失', () => {
+		equal(ca('0.1+0.2'), 0.3)
+	});
+	it('空格不影响运算', () => {
+		equal(ca('  0.1   +   0.5   '), 0.6);
+	});
+});
+
+describe('不合法情况测试', () => {
+	it('输入包含非法字符', () => {
+		
+	})
+});
